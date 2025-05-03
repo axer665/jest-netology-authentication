afca3716871d76301633e6faad744ae487d729ca
Misha <vellsevul@mail.ru>
2025-05-03 12:58:09 +0300

    Removed '/' from controllers

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,22 +3,22 @@ import { AuthService } from './auth.service';
 import { User } from '../infrastucture/user.interface';
 import { JwtAuthGuard } from './guard/auth.guard';
 
-@Controller('/users')
+@Controller('users')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post('/signin')
+  @Post('signin')
   signIn(@Body() data: Record<string, string>) {
     return this.authService.signIn(data.email, data.password);
   }
 
-  @Post('/signup')
+  @Post('signup')
   signUp(@Body() data: User) {
     return this.authService.signUp(data);
   }
 
   @UseGuards(JwtAuthGuard)
-  @Get('/all')
+  @Get('all')
   getUsers() {
     return this.authService.getUsers();
   }
